3faf2ec8576c21d186bddaaac2ac044afbb096df
fabian63-cyber <portalfabian63@gmail.com>
2026-09-12 00:33:28 -0300

    feat(clientes): agrega prop onNuevoCliente a FormCliente

--- src/components/FormCliente.jsx
+++ src/components/FormCliente.jsx
@@ -3,7 +3,7 @@ import { useState } from "react";
 import { Form, Button, Alert, Spinner } from "react-bootstrap";
 import clientesService from "../services/clientesService";
 
-const FormCliente = () => {
+const FormCliente = ({ onNuevoCliente }) => {
 
     const [nombre, setNombre] = useState("");
     const [email, setEmail] = useState("");
